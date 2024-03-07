7daf961dbcfd633d558d241d7f9a592da5213015
James Au <21522019@gm.uit.edu.vn>
2024-03-07 13:45:49 +0700

    feat: Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+
 import './App.css'
 import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
 
-// App.jsx
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
@@ -9,28 +9,30 @@ function App() {
 
 
   return (
-    <Container>
-      <InputGroup>
-        <FormControl
-          placeholder="Search For Artist"
-          type="input"
-          aria-label="Search for an Artist"
-          onKeyDown={ } // search function
-          onChange={ } // setSearch
-          style={{
-            width: "300px",
-            height: "35px",
-            borderWidth: "0px",
-            borderStyle: "solid",
-            borderRadius: "5px",
-            marginRight: "10px",
-            paddingLeft: "10px",
-          }}
-        />
+    <div className="App">
+      <Container>
+        <InputGroup>
+          <FormControl
+            placeholder="Search For Artist"
+            type="input"
+            aria-label="Search for an Artist"
+            onKeyDown={ } // search function
+            onChange={ } // setSearch
+            style={{
+              width: "300px",
+              height: "35px",
+              borderWidth: "0px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              marginRight: "10px",
+              paddingLeft: "10px",
+            }}
+          />
 
-        <Button onClick={{}}>Search</Button>
-      </InputGroup>
-    </Container>
+          <Button onClick={{}}>Search</Button>
+        </InputGroup>
+      </Container>
+    </div>
   )
 }
 
