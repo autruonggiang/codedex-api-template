d50dd9291c29b4cddd785584146a4f87f0444a76
James Au <21522019@gm.uit.edu.vn>
2024-03-07 13:34:14 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-
 import './App.css'
+import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
+
+// App.jsx
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
-  
+
 
   return (
-   <>
-      <h1>Codédex Project Template</h1>
-    </>
+    <Container>
+      <InputGroup>
+        <FormControl
+          placeholder="Search For Artist"
+          type="input"
+          aria-label="Search for an Artist"
+          onKeyDown={ } // search function
+          onChange={ } // setSearch
+          style={{
+            width: "300px",
+            height: "35px",
+            borderWidth: "0px",
+            borderStyle: "solid",
+            borderRadius: "5px",
+            marginRight: "10px",
+            paddingLeft: "10px",
+          }}
+        />
+
+        <Button onClick={{}}>Search</Button>
+      </InputGroup>
+    </Container>
   )
 }
 
